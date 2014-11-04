44d167939d9864bac8aed0b5c263f599899707e4
Johan Doornenbal <johan@filternet.nl>
2014-11-04 20:47:05 +0100

    tests verbeterd

--- integtests/src/test/java/nl/yodo/app/integtest/dom/YodoPersonTest.java
+++ integtests/src/test/java/nl/yodo/app/integtest/dom/YodoPersonTest.java
@@ -2,9 +2,11 @@ package nl.yodo.app.integtest.dom;
 
 import javax.inject.Inject;
 
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 import nl.yodo.app.integtest.YodoIntegrationTest;
 import nl.yodo.dom.Party.YodoPerson;
@@ -20,26 +22,35 @@ public class YodoPersonTest extends YodoIntegrationTest {
     
     public static class TestPerson extends YodoPersonTest {
         
+        private static final String LAST_NAME = "Test1";
+        private static final String MIDDLE_NAME = "van der";
+        private static final String FIRST_NAME = "T.";
+        private static final String UNIQUE_ID = "111";
+        private static final String OWNED_BY = "test1";
+        
         YodoPerson p1;
         
         @Test
         public void valuesSet() throws Exception {
             p1 = persons.allPersons().get(0);
             
-            assertThat(p1.getFirstName(), is("T."));
-            assertThat(p1.getLastName(), is("Test1"));
-
-            assertThat(p1.getMiddleName(), is("van der"));
-            
-            assertThat(p1.getUniquePartyId(), is("111"));
-            
-            assertThat(p1.getOwnedBy(), is("test1"));
+            assertThat(p1.getFirstName(), is(FIRST_NAME));
+            assertThat(p1.getLastName(), is(LAST_NAME));
+            assertThat(p1.getMiddleName(), is(MIDDLE_NAME));
+            assertThat(p1.getUniquePartyId(), is(UNIQUE_ID));            
+            assertThat(p1.getOwnedBy(), is(OWNED_BY));
             
         }
         
     }
 
     public static class NewPerson extends YodoPersonTest {
+        
+        private static final String LAST_NAME = "Test1";
+        private static final String MIDDLE_NAME = "van der";
+        private static final String FIRST_NAME = "T.";
+        private static final String UNIQUE_ID = "321";
+        private static final String OWNED_BY = "test1";
         
         YodoPerson p1;
         YodoPerson p2;
@@ -51,7 +62,7 @@ public class YodoPersonTest extends YodoIntegrationTest {
         
         @Before
         public void setUp() throws Exception {
-            p1=persons.newPerson("123", "Voornaam", "van", "Achteren", "test1");
+            p1=persons.newPerson(UNIQUE_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, OWNED_BY);
         }
         
         @Test
@@ -59,17 +70,17 @@ public class YodoPersonTest extends YodoIntegrationTest {
             Integer maxindex = persons.allPersons().size() - 1;
             p2 = persons.allPersons().get(maxindex);
             
-            assertThat(p1.getFirstName(), is("Voornaam"));
-            assertThat(p1.getLastName(), is("Achteren"));
+            assertThat(p1.getFirstName(), is(FIRST_NAME));
+            assertThat(p1.getLastName(), is(LAST_NAME));
 
-            assertThat(p1.getMiddleName(), is("van"));
+            assertThat(p1.getMiddleName(), is(MIDDLE_NAME));
             
-            assertThat(p1.getUniquePartyId(), is("123"));
+            assertThat(p1.getUniquePartyId(), is(UNIQUE_ID));
             
-            assertThat(p1.getOwnedBy(), is("test1"));
+            assertThat(p1.getOwnedBy(), is(OWNED_BY));
             
-            assertThat(p2.getUniquePartyId(), is("123"));
-            assertThat(p2.getOwnedBy(), is("test1"));
+            assertThat(p2.getUniquePartyId(), is(UNIQUE_ID));
+            assertThat(p2.getOwnedBy(), is(OWNED_BY));
             
             assertThat(p1,is(p2));
             
